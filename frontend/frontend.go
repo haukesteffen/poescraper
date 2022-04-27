72b9d5ef81c4cf9e5d9340a6be140ffc5d6960f9
jan <jan_s+github@posteo.de>
2022-04-27 19:31:37 +0200

    Reduce frontend to minimum

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -2,23 +2,15 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"log"
 	"net/http"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("index.html")
-	if err != nil {
-		panic(err)
-	}
-	t.Execute(w, nil)
-}
-
 func main() {
 	fmt.Printf("Starting server at port 8080\n")
-	http.HandleFunc("/", index)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
 	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 }
