1cc61167035f477d42c83e41739a2dfecbd8fbf5
haukesteffen <57219809+haukesteffen@users.noreply.github.com>
2022-05-01 21:47:13 +0200

    Add standalone trainer functionality

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,9 +1,16 @@
-from utils import fetch, train_model
+from utils import create_lexicon, fetch, create_new_model, train_model
+import time
 
 def main():
-    if fetch(1529423):
-        return
-    train_model()
+    itembase='amulet'
+    create_lexicon(itembase=itembase)
+    fetch(itembase=itembase, start_id=0)
+    create_new_model(itembase=itembase)
+
+    for i in range(10):
+        if fetch(itembase=itembase) is not 1:
+            train_model(itembase=itembase)
+        time.sleep(300)
 
 
 if __name__ == "__main__":
--- trainer/utils.py
+++ trainer/utils.py
@@ -4,7 +4,7 @@ import numpy as np
 import os, re
 from sqlalchemy import create_engine
 from joblib import Parallel, delayed
-
+from sklearn.model_selection import train_test_split
 
 n_jobs = os.cpu_count()
 price_chaos_per_ex = 113.0
@@ -24,6 +24,7 @@ def extract_price(price_string=None):
 def strip_digits(input_string):
     return "".join([i for i in input_string if i.isalpha() or i == " " or i == "(" or i == ")"])
 
+
 def strip_alpha(input_string):
     return "".join([i for i in input_string if i.isnumeric() or i == " " or i == "."])
 
@@ -33,7 +34,7 @@ def convert_rolls(input_string):
         return 1.0
     else:
         return np.mean([float(roll) for roll in str.split(input_string)])
-
+    
 
 def item_parser(input_item):
     item_dict = {}
@@ -52,12 +53,14 @@ def item_parser(input_item):
             item_dict[affix] = convert_rolls(value)
     return item_dict
 
-
-def fetch(start_id=None):
+def fetch(start_id=None, itembase='ring', make_machine_learnable=True, save_progress = True):
     if start_id is None:
-        with open("assets/last_id.txt", "r") as f:
-            start_id = f.read()
-    global output_df
+        try:
+            with open(f"assets/last_fetched_{itembase}.txt", "r") as f:
+                start_id = f.read()
+        except:
+            start_id = 0
+            
     # connect to database and query all items
     print("connecting to database...")
     try:
@@ -80,8 +83,7 @@ def fetch(start_id=None):
         )
 
     print("fetching data...")
-    #input_df = pd.read_sql_query("SELECT * FROM items", con=engine)
-    input_df = pd.read_sql_query(f"SELECT * FROM items WHERE id >= {start_id} AND itembase = 'ring' AND price != '';", con=engine)
+    input_df = pd.read_sql_query(f"SELECT * FROM items WHERE id >= {start_id} AND itembase = '{itembase}' AND price != '';", con=engine)
     
 
     # remove unnecessary strings and drop items priced without number
@@ -91,11 +93,85 @@ def fetch(start_id=None):
 
     # check number of new items
     n_items = len(input_df.index)
-    print(f"fetched {n_items} new items...")
+    print(f"fetched {n_items} new {itembase} items...")
     if n_items < 32:
         print("too few items to train model. aborting...")
         return 1
 
+    if make_machine_learnable:
+        convert(input_df=input_df, itembase=itembase)
+        if save_progress:
+            # save last item id
+            last_id = input_df.iloc[-1].loc['id']
+            with open(f"assets/last_fetched_{itembase}.txt", "w") as f:
+                f.write(str(last_id))
+        
+    return input_df
+
+def convert(input_df, itembase): 
+    output_df = parse(input_df)
+    output_df = pd.concat(
+            [output_df.drop(columns=["basetype"]), pd.get_dummies(output_df["basetype"])],
+            axis=1,
+        )
+        
+    # load lexicon
+    try:
+        lexicon = pd.read_pickle(f"assets/lexicon_{itembase}.pkl")
+    except:
+        print(f'no lexicon found. creating new {itembase} lexicon...')
+        create_lexicon(itembase=itembase)
+        lexicon = pd.read_pickle(f"assets/lexicon_{itembase}.pkl")
+        print(f'created new lexicon. please retrain the model.')
+        return 1
+
+    # merge dataframes
+    output_df = pd.concat([lexicon, output_df], join="outer")
+
+    # verify fetched data
+    if len(output_df.columns) != len(lexicon.columns):
+        print('lexicon does not match, maybe some new mods were found.')
+        print('updating lexicon...')
+        create_lexicon(itembase=itembase)
+        print('created new lexicon. please retrain the model.')
+        return 1
+        
+    # save dataframe to csv file
+    print("saving data as pickle...")
+    output_df.to_pickle(f"assets/new_items_{itembase}.pkl")
+
+    return output_df
+
+def train_model(itembase='ring', save_model=True):
+    df = (
+        pd.read_pickle(f"assets/new_items_{itembase}.pkl")
+        .drop(columns=["timestamp"])
+    )
+    df["corrupted"] = df["corrupted"].astype(float)
+
+    X = df.drop(columns=["price"]).astype(float).to_numpy()
+    y = df["price"].astype(float).to_numpy()
+
+    model = tf.keras.models.load_model(f'assets/model_{itembase}')
+    model.fit(X, y, epochs=3, batch_size=32)
+    model.save(f"assets/model_{itembase}")
+
+    # save last item id
+    with open(f"assets/last_fetched_{itembase}.txt", "r") as f:
+        with open(f"assets/last_trained_{itembase}.txt", "w") as g:
+            g.write(str(f.read()))
+
+def create_lexicon(itembase):
+    input_df = fetch(start_id=0, itembase=itembase, make_machine_learnable=False)
+    output_df = parse(input_df)
+    output_df = pd.concat(
+            [output_df.drop(columns=["basetype"]), pd.get_dummies(output_df["basetype"])],
+            axis=1,
+    )
+    lexicon = pd.DataFrame(columns=output_df.columns)
+    lexicon.to_pickle(f'assets/lexicon_{itembase}.pkl')
+
+def parse(input_df):
     # parse input dataframe into output dataframe
     print("parsing items into machine learnable form...")
     item_list = Parallel(n_jobs=n_jobs)(
@@ -103,33 +179,44 @@ def fetch(start_id=None):
     )
     output_df = pd.DataFrame(item_list).fillna(0.0).set_index("itemid")
     output_df = output_df.loc[output_df.index.drop_duplicates()]
+    return output_df
 
-    # load lexicon and merge dataframes
-    lexicon = pd.read_pickle("assets/lexicon.pkl")
-    output_df = pd.concat([lexicon, output_df], join="outer")
-
-    # save dataframe to csv file
-    print("saving data as pickle...")
-    output_df.to_pickle("assets/new_items.pkl")
-
-    # save last item id
-    last_id = input_df.iloc[-1].loc['id']
-    with open("assets/last_id.txt", "w") as f:
-        f.write(str(last_id))
-    return 0
-
-
-def train_model(save_model=True):
+def create_new_model(itembase):
     df = (
-        pd.read_pickle("assets/new_items.pkl")
-        .set_index("itemid")
+        pd.read_pickle(f"assets/new_items_{itembase}.pkl")
         .drop(columns=["timestamp"])
     )
     df["corrupted"] = df["corrupted"].astype(float)
-
-    X = df.drop(columns=["price", "basetype"]).astype(float).to_numpy()
+    X = df.drop(columns=['price']).astype(float).to_numpy()
     y = df["price"].astype(float).to_numpy()
 
-    model = tf.keras.models.load_model('assets/model')
-    model.fit(X, y, epochs=3, batch_size=32)
-    model.save("assets/model")
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.1, random_state=42
+    )
+
+    early_stopping = tf.keras.callbacks.EarlyStopping(
+        monitor="val_loss",
+        min_delta=0,
+        patience=20,
+        verbose=0,
+        mode="auto",
+        baseline=None,
+        restore_best_weights=True,
+    )
+    
+    lexicon = pd.read_pickle(f'assets/lexicon_{itembase}.pkl')
+    n_features = len(lexicon.columns) - 2
+
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Dense(400, activation="relu", input_shape=(n_features,)))
+    model.add(tf.keras.layers.Dense(400, activation="relu"))
+    model.add(tf.keras.layers.Dense(400, activation="relu"))
+    model.add(tf.keras.layers.Dense(1, activation="relu"))
+    model.compile(optimizer="adam", loss="mae")
+    model.fit(X_train, y_train, epochs=200, batch_size=32, validation_data=(X_test, y_test), callbacks=[early_stopping])
+    model.save(f'assets/model_{itembase}')
+
+    # save last item id
+    with open(f"assets/last_fetched_{itembase}.txt", "r") as f:
+        with open(f"assets/last_trained_{itembase}.txt", "w") as g:
+            g.write(str(f.read()))
