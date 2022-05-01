087fc759f1a6621079e9e06e1c7d39a515b145df
jan <jan_s+github@posteo.de>
2022-05-01 22:56:49 +0200

    Collect all items

--- scraper/main.go
+++ scraper/main.go
@@ -109,23 +109,21 @@ func stashParser(stashes api.Poe) {
 				if item.FrameType > 0 && item.FrameType < 3 && item.Identified {
 					subcat = item.Extended.Subcategories
 					if len(subcat) > 0 {
-						if subcat[0] == "ring" || subcat[0] == "amulet" || subcat[0] == "belt" {
-							if todb {
-								itemToDB(item)
-							} else {
-								//fmt.Printf("%+v", item)
-								fmt.Printf("%v\nRarity: %v\niLvl: %v\n", item.BaseType, rarity[item.FrameType], item.Ilvl)
-								for _, imp := range item.ImplicitMods {
-									fmt.Println(imp)
-								}
-								fmt.Println("-----------------")
-								for _, aff := range item.ExplicitMods {
-									fmt.Println(aff)
-								}
-								fmt.Println("-----------------")
-								fmt.Printf("Price: %v\n", item.Note)
-								fmt.Println()
+						if todb {
+							itemToDB(item)
+						} else {
+							//fmt.Printf("%+v", item)
+							fmt.Printf("%v\nRarity: %v\niLvl: %v\n", item.BaseType, rarity[item.FrameType], item.Ilvl)
+							for _, imp := range item.ImplicitMods {
+								fmt.Println(imp)
 							}
+							fmt.Println("-----------------")
+							for _, aff := range item.ExplicitMods {
+								fmt.Println(aff)
+							}
+							fmt.Println("-----------------")
+							fmt.Printf("Price: %v\n", item.Note)
+							fmt.Println()
 						}
 					}
 				}
