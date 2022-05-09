65d3ac18d3bb8e9c3d0dc1cd752833c9558ddcb7
jan <jan_s+github@posteo.de>
2022-05-09 13:54:35 +0200

    Print time of unexpected response

--- scraper/main.go
+++ scraper/main.go
@@ -79,7 +79,7 @@ func fetchapi(client *resty.Client, change_id string) (string, float64) {
 		go stashParser(dump)
 		return dump.NextChangeID, float64(resp.Size()) / (1 << 20)
 	} else if respCode == 429 {
-		fmt.Println("Status Code:", respCode, "at", resp.Time())
+		fmt.Println("Status Code:", respCode, "at", resp.ReceivedAt())
 		dumpHeaders(resp)
 		fmt.Println(resp)
 		header := resp.Header().Get("Retry-After")
@@ -93,7 +93,7 @@ func fetchapi(client *resty.Client, change_id string) (string, float64) {
 		return change_id, 0
 	} else {
 		fmt.Println("Other Error. Sleeping 60s")
-		fmt.Println(resp.StatusCode(), "at", resp.Time())
+		fmt.Println(resp.StatusCode(), "at", resp.ReceivedAt())
 		fmt.Println(resp)
 		time.Sleep(time.Duration(60) * time.Second)
 		return change_id, 0
